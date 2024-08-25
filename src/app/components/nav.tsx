1edf73658edb152a77546c34da51718bab4029f3
Peter Cho <pcho51990@gmail.com>
2024-08-24 22:15:56 -0400

    mobile nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,35 +2,75 @@
 
 import Image from "next/image";
 import NavLink from "@/app/components/nav-link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
 
 export default function Nav() {
   const [hovered, setHovered] = useState(false);
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
   const pathname = usePathname();
+  const router = useRouter();
   const isContact = pathname.startsWith("/contact");
 
   const handleHoverOn = () => {
+    document.body.style.overflow = "hidden";
     setHovered(true);
   };
 
   const handleHoverOff = () => {
+    document.body.style.overflow = "auto";
     setHovered(false);
   };
+
+  const openMobileNav = () => {
+    document.body.style.overflow = "hidden";
+    setMobileNavVisible(true);
+  };
+
+  const closeMobileNav = () => {
+    document.body.style.overflow = "auto";
+    setMobileNavVisible(false);
+  };
+
+  const handleMobileNavLink = (path: string) => {
+    closeMobileNav();
+    router.push(path);
+  };
+
+  const generateProjectLinks = (path: string, name: string) => (
+    <div onMouseOver={handleHoverOff}>
+      <NavLink
+        href={path}
+        className={`${pathname === path ? "font-bold" : ""}`}
+      >
+        {name}
+      </NavLink>
+    </div>
+  );
+
+  const generateProjectCard = (project: string) => (
+    <Link key={project} href={`/projects/${project}`} onClick={handleHoverOff}>
+      <Image
+        src={`/${project}/logo-md.svg`}
+        alt={project}
+        width={338}
+        height={200}
+      />
+    </Link>
+  );
+
+  const contactHoverClass =
+    isContact && !hovered
+      ? "bg-transparent transition-all ease-in duration-700"
+      : "bg-base";
 
   return (
     <div className="relative mt-[10px] lg:mt-0 lg:block">
       <div
         className={
           "absolute top-0 right-0 left-0 flex justify-between items-center lg:p-16 lg:h-[192px] z-20 " +
-          `${
-            isContact
-              ? hovered
-                ? "bg-base"
-                : "bg-transparent transition-all ease-in duration-700"
-              : "bg-base"
-          } `
+          `${contactHoverClass} `
         }
       >
         <div className="mx-auto lg:mx-0 w-[75px] lg:w-[155px] h-[50px] lg:h-[105px] relative">
@@ -39,44 +79,16 @@ export default function Nav() {
           </Link>
         </div>
         <div className="hidden lg:flex space-x-[4.5rem] font-light">
-          <div onMouseOver={handleHoverOff}>
-            <NavLink
-              href="/"
-              className={`${pathname === "/" ? "font-bold" : ""}`}
-            >
-              Home
-            </NavLink>
-          </div>
-          <div onMouseOver={handleHoverOff}>
-            <NavLink
-              href="/about"
-              className={`${pathname === "/about" ? "font-bold" : ""}`}
-            >
-              About
-            </NavLink>
-          </div>
+          {generateProjectLinks("/", "Home")}
+          {generateProjectLinks("/about", "About")}
           <div
             onMouseOver={handleHoverOn}
             className="text-2xl tracking-widest hover:cursor-pointer"
           >
             Projects
           </div>
-          <div onMouseOver={handleHoverOff}>
-            <NavLink
-              href="/resume"
-              className={`${pathname === "/resume" ? "font-bold" : ""}`}
-            >
-              Resume
-            </NavLink>
-          </div>
-          <div onMouseOver={handleHoverOff}>
-            <NavLink
-              href="/contact"
-              className={`${pathname === "/contact" ? "font-bold" : ""}`}
-            >
-              Contact
-            </NavLink>
-          </div>
+          {generateProjectLinks("/resume", "Resume")}
+          {generateProjectLinks("/contact", "Contact")}
         </div>
       </div>
       <div
@@ -87,45 +99,49 @@ export default function Nav() {
         onMouseLeave={handleHoverOff}
       >
         <div className="flex items-center justify-around h-[252px]">
-          <Link href="/projects/resto" onClick={handleHoverOff}>
-            <Image
-              src="/resto/logo-md.svg"
-              alt="Resto"
-              width={338}
-              height={200}
-            />
-          </Link>
-          <Link href="/projects/korean-air" onClick={handleHoverOff}>
-            <Image
-              src="/korean-air/logo-md.svg"
-              alt="Korean Air"
-              width={338}
-              height={200}
-            />
-          </Link>
-          <Link href="/projects/pibu" onClick={handleHoverOff}>
-            <Image
-              src="/pibu/logo-md.svg"
-              alt="Pibu"
-              width={338}
-              height={200}
-            />
-          </Link>
+          {["resto", "korean-air", "pibu"].map(generateProjectCard)}
         </div>
       </div>
       {/* overlay */}
       <div
-        className={
-          "absolute lg:h-[2000px] lg:w-[1440px] z-10 transition-all duration-500 bg-black top-0 bottom-0 left-0 right-0 pointer-events-none " +
-          `${hovered ? "opacity-50" : "opacity-0"}`
-        }
+        className={`absolute lg:h-[2000px] lg:w-[1440px] z-10 transition-all duration-500 bg-black top-0 bottom-0 left-0 right-0 pointer-events-none ${
+          hovered ? "opacity-20" : "opacity-0"
+        }`}
         onMouseEnter={handleHoverOn}
       ></div>
-      {/* mobile nav */}
-      <div className="absolute left-[20px] w-[30px] h-[50px] flex justify-center items-center z-40 lg:hidden">
+      {/* hamburger */}
+      <div
+        className="absolute left-[20px] w-[30px] h-[50px] flex justify-center items-center z-40 lg:hidden"
+        onClick={openMobileNav}
+      >
         <div className="w-full h-full">
-        <Image src="/icons/hamburger.svg" alt="menu" fill />
+          <Image src="/icons/hamburger.svg" alt="menu" fill />
         </div>
+      </div>
+      {/* mobile nav */}
+      <div
+        className={`${
+          mobileNavVisible
+            ? "translate-x-0"
+            : "-translate-x-full pointer-events-none"
+        } fixed w-[100vw] h-[100vh] z-40 bg-black top-0 bottom-0 left-0 right-0 opacity-20`}
+        onClick={closeMobileNav}
+      ></div>
+      <div
+        className={
+          `fixed w-[60vw] h-[100vh] z-50 bg-base top-0 bottom-0 left-0 right-0 text-[16px] tracking-[.02em] space-y-4 py-6 px-5 ` +
+          `${
+            mobileNavVisible
+              ? "translate-x-0"
+              : "-translate-x-full pointer-events-none"
+          } transition-all duration-300`
+        }
+      >
+        <div onClick={() => handleMobileNavLink("/")}>Home</div>
+        <div onClick={() => handleMobileNavLink("/about")}>About</div>
+        <div onClick={() => handleMobileNavLink("/projects")}>Projects</div>
+        <div onClick={() => handleMobileNavLink("/resume")}>Resume</div>
+        <div onClick={() => handleMobileNavLink("/contact")}>Contact</div>
       </div>
     </div>
   );
