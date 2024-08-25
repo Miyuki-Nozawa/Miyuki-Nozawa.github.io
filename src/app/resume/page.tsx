dd4831fd8a0aa29e2234bdcb3ad2f4fac4c27b59
Peter Cho <pcho51990@gmail.com>
2024-08-24 21:31:51 -0400

    resume mobile

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,14 +4,9 @@ import Image from "next/image";
 export default function Resume() {
   return (
     <main>
-      <div className="space-y-12 w-[1320px] mx-auto pt-20 pb-32">
-        <Image src="/resume.png" alt="resume" width={1320} height={1844} />
-        <div
-          className={
-            "bg-brown px-16 py-8 rounded-lg text-[22px] font-medium tracking-widest " +
-            "ml-auto w-[314px] text-white"
-          }
-        >
+      <div className="space-y-3 lg:space-y-12 lg:w-[1320px] mx-auto px-5 lg:px-16 pt-[90px] lg:pt-20 pb-20 lg:pb-32">
+        <Image src="/resume.png" alt="resume" width={1320} height={1844} className="border-[.265px] border-black" />
+        <div className="bg-brown h-[45px] lg:px-16 lg:py-8 rounded-lg text-[18px] lg:text-[22px] font-medium tracking-widest ml-auto w-full lg:w-[314px] text-white flex items-center justify-center">
           <Link href="/resume.pdf" target="_blank">
             Download PDF
           </Link>
