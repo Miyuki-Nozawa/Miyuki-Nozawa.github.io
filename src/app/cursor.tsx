ca7d1d558628be6d98107e98c642633264645098
Peter Cho <pcho51990@gmail.com>
2024-10-05 22:11:39 -0400

    fixes

--- src/app/cursor.tsx
+++ src/app/cursor.tsx
@@ -1,13 +1,13 @@
 export const handleCursorHoverStop = () => {
   const cursorWrapper = document.querySelector(".cursor-wrapper");
   if (cursorWrapper) {
-    cursorWrapper.classList.add("hovering");
+    cursorWrapper.classList.remove("hovering");
   }
 };
 
 export const handleCursorHoverStart = () => {
   const cursorWrapper = document.querySelector(".cursor-wrapper");
   if (cursorWrapper) {
-    cursorWrapper.classList.remove("hovering");
+    cursorWrapper.classList.add("hovering");
   }
 };
