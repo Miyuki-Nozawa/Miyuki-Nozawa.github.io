7fc781ee8da412198de2f80b5255a9985ef1af52
Peter Cho <pcho51990@gmail.com>
2024-10-06 01:46:51 -0400

    pointer cursor

--- src/app/cursor.tsx
+++ src/app/cursor.tsx
@@ -2,6 +2,7 @@ export const handleCursorHoverStop = () => {
   const cursorWrapper = document.querySelector(".cursor-wrapper");
   if (cursorWrapper) {
     cursorWrapper.classList.remove("hovering");
+    document.body.classList.remove("hovering");
   }
 };
 
@@ -9,5 +10,6 @@ export const handleCursorHoverStart = () => {
   const cursorWrapper = document.querySelector(".cursor-wrapper");
   if (cursorWrapper) {
     cursorWrapper.classList.add("hovering");
+    document.body.classList.add("hovering");
   }
 };
